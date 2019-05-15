da9c8b66efc312b5cf0f904e89229ef7f8bffa88
Serhii Riznychenko <magnesijium@gmail.com>
2019-05-15 03:22:13 +0300

    Add secretKey into config package object

--- src/main/scala/io/github/serjorez/scurrency/config/package.scala
+++ src/main/scala/io/github/serjorez/scurrency/config/package.scala
@@ -2,8 +2,12 @@ package io.github.serjorez.scurrency
 
 import io.circe.Decoder
 import io.circe.generic.semiauto._
+import io.circe.config.syntax._
 
 package object config {
+
+  val secretKey: String = sys.env("CMC_SECRET_KEY")
+
   implicit val jobsDec: Decoder[JobsConfig] = deriveDecoder
   implicit val frDec: Decoder[FetchRatesConfig] = deriveDecoder
   implicit val rcDec: Decoder[RatesCleanupConfig] = deriveDecoder
