bfaa36c62d2513e95c07454fd76abf3efd1a9a95
Serhii Riznychenko <magnesijium@gmail.com>
2019-05-15 10:45:54 +0300

    Refactor FetchRatesJob class, found bug in yield statement

--- src/main/scala/io/github/serjorez/scurrency/jobs/FetchRatesJob.scala
+++ src/main/scala/io/github/serjorez/scurrency/jobs/FetchRatesJob.scala
@@ -10,27 +10,27 @@ import io.github.serjorez.scurrency.domain.{CryptocurrencyListing, Cryptocurrenc
 import io.github.serjorez.scurrency.domain.CryptocurrencyRates.cryptocurrencyRatesSemigroup
 import io.github.serjorez.scurrency.utils.JsonUtils.parseF
 import io.circe.generic.auto._
-import io.github.serjorez.scurrency.config.JsonConfig.TimestampFormat
 
 import scala.concurrent.duration._
 import scala.concurrent.duration.FiniteDuration
 
-class FetchRatesJob[F[_] : Async](fetchRatesConfig: FetchRatesConfig,
-                                  fileActions: FileActionsAlgebraInterpreter[F, CryptocurrencyRates],
-                                  uri: String,
-                                  params: Seq[(String, String)],
-                                  backoffDelay: FiniteDuration,
-                                  maxRetries: Int)
-                                 (implicit backend: SttpBackend[F, Nothing], timer: Timer[F])
+class FetchRatesJob[F[_] : Sync](fetchRatesConfig: FetchRatesConfig,
+                                 fileActions: FileActionsAlgebraInterpreter[F, CryptocurrencyRates],
+                                 uri: String,
+                                 params: Seq[(String, String)],
+                                 backoffDelay: FiniteDuration,
+                                 maxRetries: Int)
+                                (implicit backend: SttpBackend[F, Nothing], timer: Timer[F])
   extends JobAlgebra[F] {
 
-  override def run: F[Unit] = if (fetchRatesConfig.enabled) runInfinitly() else Async[F].unit
+  override def run: F[Unit] = if (fetchRatesConfig.enabled) runInfinitly() else Sync[F].unit
 
   def runInfinitly(coldStart: Boolean = true): F[Unit] = for {
     listing <- getCryptocurrencyListing
-    _       <- if(coldStart) writeCryptocurrencyRates(listing) else appendCryptocurrencyRates(listing)
+    _       <- if (coldStart) writeCryptocurrencyRates(listing) else appendCryptocurrencyRates(listing)
     _       <- timer.sleep(fetchRatesConfig.schedule.interval)
-  } yield Async[F].defer(runInfinitly(false))
+    _       <- Sync[F].defer(runInfinitly(false))
+  } yield ()
 
   def writeCryptocurrencyRates(listing: CryptocurrencyListing): F[Unit] =
     fileActions.write(CryptocurrencyRates(listing))
@@ -44,26 +44,26 @@ class FetchRatesJob[F[_] : Async](fetchRatesConfig: FetchRatesConfig,
 
     retryWithBackoff(request.send, delay = backoffDelay, maxRetries).flatMap { response =>
       response.body match {
-        case Right(rawBody)     => parseF(rawBody)
+        case Right(rawBody)     => parseF[F, CryptocurrencyListing](rawBody)
         case Left(errorMessage) =>
-          Async[F].raiseError[CryptocurrencyListing](
+          Sync[F].raiseError[CryptocurrencyListing](
             new Exception(s"Failed to get cryptocurrency listing. Invalid response: (${response.code}) $errorMessage"))
       }
     }
   }
 
-  def retryWithBackoff[A, F[_] : Sync](fa: F[A],
-                                       delay: FiniteDuration,
-                                       maxRetries: Int)
-                                      (implicit timer: Timer[F]): F[A] =
+  def retryWithBackoff[A](fa: F[A],
+                          delay: FiniteDuration,
+                          maxRetries: Int)
+                         (implicit timer: Timer[F]): F[A] =
     fa.handleErrorWith { error =>
-      if (maxRetries > 0) timer.sleep(delay) *> retryWithBackoff(fa, delay * 2, maxRetries - 1)
+      if (maxRetries > 0) timer.sleep(delay) *> retryWithBackoff(fa, delay, maxRetries - 1)
       else ApplicativeError[F, Throwable].raiseError(error)
     }
 }
 
 object FetchRatesJob {
-  def apply[F[_] : Async](fetchRatesConfig: FetchRatesConfig,
+  def apply[F[_] : Sync](fetchRatesConfig: FetchRatesConfig,
                           fileActions: FileActionsAlgebraInterpreter[F, CryptocurrencyRates],
                           uri: String = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
                           params: Seq[(String, String)] = Seq(("start", "1"), ("limit", "3"), ("convert", "USD")),
