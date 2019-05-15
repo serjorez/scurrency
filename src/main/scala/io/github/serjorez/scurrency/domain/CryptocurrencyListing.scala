c19d872dbab39a7a49566ae84088d69dbf9e2679
Serhii Riznychenko <magnesijium@gmail.com>
2019-05-15 06:33:38 +0300

    Remove parseListing function from CryptocurrencyListing object

--- src/main/scala/io/github/serjorez/scurrency/domain/CryptocurrencyListing.scala
+++ src/main/scala/io/github/serjorez/scurrency/domain/CryptocurrencyListing.scala
@@ -1,25 +1,4 @@
 package io.github.serjorez.scurrency.domain
 
-import cats.effect.Async
-import io.circe.parser.parse
-import io.circe.generic.auto._
-import io.github.serjorez.scurrency.config.JsonConfig.TimestampFormat
-
 case class CryptocurrencyListing(status: CryptocurrencyListingStatus,
                                  data: List[Cryptocurrency])
-
-object CryptocurrencyListing {
-
-  def parseListing[F[_]: Async](mayBeJson: String): F[CryptocurrencyListing] = {
-    parse(mayBeJson) match {
-      case Right(json) =>
-        json.as[CryptocurrencyListing] match {
-          case Right(listing)        => Async[F].delay(listing)
-          case Left(decodingFailure) => Async[F].raiseError[CryptocurrencyListing](
-            new Exception(s"Failed to decode JSON output: ${decodingFailure.message}"))
-        }
-      case Left(parsingFailure) => Async[F].raiseError[CryptocurrencyListing](
-        new Exception(s"Failed to parse JSON output: ${parsingFailure.message}", parsingFailure.underlying))
-    }
-  }
-}
